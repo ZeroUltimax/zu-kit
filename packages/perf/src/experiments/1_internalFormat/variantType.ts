85983d877d83ce938b5f131171167c1097791ab0
ZeroUltimax <antoinerobertson92@gmail.com>
2026-03-14 16:51:01 -0400

    wip

--- packages/perf/src/experiments/1_internalFormat/variantType.ts
+++ packages/perf/src/experiments/1_internalFormat/variantType.ts
@@ -1,27 +1,13 @@
-export interface ResultFormatVariant<
-  SUCC_VAL = any,
-  SUCC_TYPE = any,
-  FAIL_VAL = any,
-  FAIL_TYPE = any,
-> {
+export interface ResultFormatVariant<SUCC_VAL = any, SUCC_TYPE = any, FAIL_VAL = any, FAIL_TYPE = any> {
   succ: (v: SUCC_VAL) => SUCC_TYPE;
   fail: (v: FAIL_VAL) => FAIL_TYPE;
   isSucc: (r: SUCC_TYPE | FAIL_TYPE) => r is SUCC_TYPE;
   isFail: (r: SUCC_TYPE | FAIL_TYPE) => r is FAIL_TYPE;
   get: (s: SUCC_TYPE) => SUCC_VAL;
   getFail: (f: FAIL_TYPE) => FAIL_VAL;
-  map: (
-    r: SUCC_TYPE | FAIL_TYPE,
-    proj: (s: SUCC_VAL) => SUCC_VAL,
-  ) => SUCC_TYPE | FAIL_TYPE;
-  andThen: (
-    a: SUCC_TYPE | FAIL_TYPE,
-    b: (s: SUCC_VAL) => SUCC_TYPE | FAIL_TYPE,
-  ) => SUCC_TYPE | FAIL_TYPE;
-  orElse: (
-    a: SUCC_TYPE | FAIL_TYPE,
-    b: (f: FAIL_VAL) => SUCC_TYPE | FAIL_TYPE,
-  ) => SUCC_TYPE | FAIL_TYPE;
+  map: (r: SUCC_TYPE | FAIL_TYPE, proj: (s: SUCC_VAL) => SUCC_VAL) => SUCC_TYPE | FAIL_TYPE;
+  andThen: (a: SUCC_TYPE | FAIL_TYPE, b: (s: SUCC_VAL) => SUCC_TYPE | FAIL_TYPE) => SUCC_TYPE | FAIL_TYPE;
+  orElse: (a: SUCC_TYPE | FAIL_TYPE, b: (f: FAIL_VAL) => SUCC_TYPE | FAIL_TYPE) => SUCC_TYPE | FAIL_TYPE;
 }
 
 export type NumVariant<SS, FF> = ResultFormatVariant<number, SS, number, FF>;
